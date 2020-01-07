e4951d89495c8b2813116ada54698c1c0f257086
xyfy <benguahao@foxmail.com>
2020-01-07 18:25:29 +0800

    添加项目文件。

---
+++ Trojan.Tran.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using Trojan.Tray;
+
+namespace Trojan.Tran.Wpf
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window, IDisposable
+    {
+        TrojanHelper helper = new TrojanHelper();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ResizeMode = ResizeMode.CanMinimize;
+            helper.DataReceivedEvent += Helper_DataReceivedEvent;
+            if (File.Exists(helper.ExePath))
+            {
+                btnStart.IsEnabled = true;
+                lblCursorPosition.Text = $"运行文件路径：{helper.ExePath}";
+            }
+            else
+            {
+                btnStart.IsEnabled = false;
+                lblCursorPosition.Text = $"运行文件不存在：{helper.ExePath}"; ;
+            }
+            btnEnd.IsEnabled = false;
+        }
+
+        private void Helper_DataReceivedEvent(string text, DataReceivedType dataReceivedType)
+        {
+            UpdateListBox(text);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            helper.Close();
+            base.OnClosing(e);
+        }
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                helper.Start();
+                btnEnd.IsEnabled = true;
+                btnStart.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "发生错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        void UpdateListBox(string text)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                if (lbInfos.Items.Count > 5000)
+                {
+                    lbInfos.Items.Clear();
+                }
+                lbInfos.Items.Add(text);
+            }));
+        }
+
+        private void btnEnd_Click(object sender, RoutedEventArgs e)
+        {
+            helper.Close();
+            btnEnd.IsEnabled = false;
+            btnStart.IsEnabled = true;
+        }
+        private bool disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.disposed) return;
+
+            if (disposing)
+            {
+                helper.Dispose();
+            }
+
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Trojan.Tray.Base/TrojanHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Trojan.Tray
+{
+
+
+    public class TrojanHelper : IDisposable
+    {
+        Process process = null;
+        private static char DirectorySeparatorChar => Path.DirectorySeparatorChar;
+        string exePath = null;
+        string workDir = null;
+
+
+        public string ExePath
+        {
+            get
+            {
+                return exePath;
+            }
+        }
+
+        public delegate void DataReceived(string text, DataReceivedType dataReceivedType);
+
+        public event DataReceived DataReceivedEvent;
+
+        public TrojanHelper()
+        {
+            workDir = $"{Environment.CurrentDirectory}{ DirectorySeparatorChar }trojan";
+            exePath = System.IO.Path.Combine(workDir, $"trojan.exe");
+        }
+        public void Start()
+        {
+            process = new Process();
+            var processStartInfo = process.StartInfo;
+            processStartInfo.WorkingDirectory = workDir;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.FileName = exePath;
+            process.OutputDataReceived += Process_OutputDataReceived;
+            process.ErrorDataReceived += Process_ErrorDataReceived;
+            process.Start();
+            process.BeginOutputReadLine();//开始读取输出数据
+            process.BeginErrorReadLine();//开始读取错误数据，重要！
+        }
+
+        private void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                this.DataReceivedEvent(e.Data, DataReceivedType.Error);
+            }
+        }
+
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                this.DataReceivedEvent(e.Data, DataReceivedType.Output);
+            }
+        }
+
+        public void Close()
+        {
+            if (process != null && !process.HasExited)
+            {
+                process.Kill();
+            }
+        }
+        public void Dispose()
+        {
+            if (process != null && !process.HasExited)
+            {
+                process.Dispose();
+            }
+        }
+    }
+}
